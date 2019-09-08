53afc3031f89d478f90d9b2601b4af7eb8724a75
Antal Raffai <raffai.antal@gmail.com>
2019-09-08 09:49:39 +0300

    getters and setters

--- src/main/java/ee/sda/mackirill/entities/Person.java
+++ src/main/java/ee/sda/mackirill/entities/Person.java
@@ -14,4 +14,61 @@ public class Person {
     private String password;
     private String phoneNumber;
     private int type_id;
+
+    public Person(int id, String name, String email, String password, String phoneNumber, int type_id) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.type_id = type_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getType_id() {
+        return type_id;
+    }
+
+    public void setType_id(int type_id) {
+        this.type_id = type_id;
+    }
 }
