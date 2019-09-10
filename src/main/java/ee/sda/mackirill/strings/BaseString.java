43309958dcc6c43dedbd9ca373ce2a023c851050
klyappe <gulhos@gmail.com>
2019-09-10 15:22:12 +0300

    Add Menu items save

--- src/main/java/ee/sda/mackirill/strings/BaseString.java
+++ src/main/java/ee/sda/mackirill/strings/BaseString.java
@@ -9,4 +9,5 @@ public class BaseString extends ConsoleColors {
     public static final String WELCOME = "Welcome to MacKirill restaurant management system";
     public static final String WRONG_COMMAND = RED + "Command is wrong. Please print correct command." + RESET;
     public static final String O_RETURN_BACK = "0. return back.";
+    public static final String SAVE_IN_DB = GREEN + "Saved in databse" + RESET;
 }
