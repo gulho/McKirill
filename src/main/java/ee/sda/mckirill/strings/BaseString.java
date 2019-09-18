ee595753804a5b0ec1ad47f49127a6dca370024e
klyappe <gulhos@gmail.com>
2019-09-18 09:15:37 +0300

    Add testing for Big decimal

--- src/main/java/ee/sda/mckirill/strings/BaseString.java
+++ src/main/java/ee/sda/mckirill/strings/BaseString.java
@@ -12,7 +12,7 @@ public class BaseString extends ConsoleColors {
     public static final String WELCOME = "Welcome to MacKirill restaurant management system";
     public static final String WRONG_COMMAND = RED + "Command is wrong. Please print correct command." + RESET;
     public static final String O_RETURN_BACK = "0. return back.";
-    public static final String SAVE_IN_DB = GREEN + "Saved in databse" + RESET;
+    public static final String SAVE_IN_DB = GREEN + "Saved in database" + RESET;
 
     public static final String NOT_LOGIN = RED + "Yo do not log in" + RESET;
 
@@ -21,5 +21,7 @@ public class BaseString extends ConsoleColors {
 
     public static final String SELECT_ID_NOT_INTEGER = RED + "Typed value not integer" + RESET;
     public static final String SELECT_STRING_TO_LONG = RED + "Typed string to long. Maximum length: " + RESET;
+
+    public static final String NUMBER_FORMAT_EXCEPTION = RED + "Number format is incorrect" +RESET;
 
 }
