94d47c72ce8c2158ca15dfe11254695596716712
klyappe <gulhos@gmail.com>
2019-09-18 11:20:42 +0300

    Add ID testing

--- src/main/java/ee/sda/mckirill/strings/BaseString.java
+++ src/main/java/ee/sda/mckirill/strings/BaseString.java
@@ -19,7 +19,7 @@ public class BaseString extends ConsoleColors {
     public static final String TABLE_ID = "ID";
     public static final String TABLE_PERSON = "Person name";
 
-    public static final String SELECT_ID_NOT_INTEGER = RED + "Typed value not integer" + RESET;
+    public static final String SELECT_ID_NOT_INTEGER = RED + "Typed value not integer or less than 1" + RESET;
     public static final String SELECT_STRING_TO_LONG = RED + "Typed string to long. Maximum length: " + RESET;
 
     public static final String NUMBER_FORMAT_EXCEPTION = RED + "Number format is incorrect" +RESET;
