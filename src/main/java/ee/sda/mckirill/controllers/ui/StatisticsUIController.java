56d0a0c60dc056d73df493dbe7bdf3c3b3dd397e
klyappe <gulhos@gmail.com>
2019-09-24 16:31:16 +0300

    Statistics stage 3

--- src/main/java/ee/sda/mckirill/controllers/ui/StatisticsUIController.java
+++ src/main/java/ee/sda/mckirill/controllers/ui/StatisticsUIController.java
@@ -1,6 +1,5 @@
 package ee.sda.mckirill.controllers.ui;
 
-import ee.sda.mckirill.entities.Order;
 import ee.sda.mckirill.entities.Person;
 import ee.sda.mckirill.strings.StatisticsString;
 
@@ -14,19 +13,15 @@ public class StatisticsUIController extends AbstractUIController {
 
     @Override
     public void start() {
-/*        Map<Integer, Consumer> clientActions = new HashMap<>();
-        clientActions.put(1, T -> System.out.println(StatisticsString.TODO));
-        clientActions.put(2, T -> System.out.println(StatisticsString.TODO));
-        clientActions.put(3, T -> System.out.println(StatisticsString.TODO));
-
-        selectMenuAction(StatisticsString.STATISTICS_MAIN_ACTION, clientActions);*/
 
         System.out.println(StatisticsString.ORDERS_COUNT_IN_ALL_TIME
-                + getListFromNamedQueryWithParameters("orders_count_by_date",
+                + getListFromNamedQueryWithParameters("orders_count_older_than_date",
                 Object.class, Map.of("timeToOrder", LocalDateTime.MIN)).get(0));
-    }
 
-    private void ordersStatistics() {
+        System.out.println(StatisticsString.ORDER_FOR_TODAY
+                + getListFromNamedQueryWithParameters("orders_count_by_day", Object.class, Map.of("timeToOrder", LocalDateTime.now())).get(0));
 
+        System.out.println(StatisticsString.CLIENTS_COUNT
+                + getListFromNamedQuery("clients_count", Object.class).get(0));
     }
 }
--- src/main/java/ee/sda/mckirill/strings/StatisticsString.java
+++ src/main/java/ee/sda/mckirill/strings/StatisticsString.java
@@ -1,11 +1,7 @@
 package ee.sda.mckirill.strings;
 
 public class StatisticsString extends BaseString {
-    public static final String STATISTICS_MAIN_ACTION = PLEASE_SELECT_ACTION
-            + "1. Orders statistics." + BR
-            + "2. Client statistics." + BR
-            + "3. Waiter statistics" + BR
-            + O_RETURN_BACK;
     public static final String ORDERS_COUNT_IN_ALL_TIME = GREEN + "All time orders: " + RESET;
     public static final String ORDER_FOR_TODAY = GREEN + "Orders for today: " + RESET;
+    public static final String CLIENTS_COUNT = GREEN + "Registered Clients count: " + RESET;
 }
